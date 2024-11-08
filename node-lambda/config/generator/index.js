d1233bec494824c96719a36d682f137ba0dfca1d
Franz Bendezu <franzant20@gmail.com>
2024-11-08 11:09:56 -0500

    fix: format generator configuration for improved readability

--- node-lambda/config/generator/index.js
+++ node-lambda/config/generator/index.js
@@ -1,31 +1,29 @@
 import { GENERATOR_ACTIONS } from "./actions";
 
-
 export const GENERATOR_CONFIG = {
-    description: "Generate a handler for a new resource",
-    prompts: [
-      {
-        type: "input",
-        name: "moduleName",
-        message: "Resource path",
-      },
-      {
-        type: "input",
-        name: "folder",
-        message: "Resource folder",
-      },
-      {
-        type: "input",
-        name: "name",
-        message: "Resource name",
-      },
-      {
-        type: "list",
-        name: "operation",
-        message: "Resource operation",
-        choices: ["create", "read", "update", "delete"],
-      },
-    ],
-    actions: GENERATOR_ACTIONS,
-  };
-  
+  description: "Generate a handler for a new resource",
+  prompts: [
+    {
+      type: "input",
+      name: "moduleName",
+      message: "Resource path",
+    },
+    {
+      type: "input",
+      name: "folder",
+      message: "Resource folder",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Resource name",
+    },
+    {
+      type: "list",
+      name: "operation",
+      message: "Resource operation",
+      choices: ["create", "read", "update", "delete"],
+    },
+  ],
+  actions: GENERATOR_ACTIONS,
+};
